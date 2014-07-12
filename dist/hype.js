56b2b6460aedc9a95dba0a8e2dba21762906ec0d
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-07-12 17:38:43 +0100

    New build

--- dist/hype.js
+++ dist/hype.js
@@ -1950,8 +1950,6 @@ var xhr = _dereq_('xhr')
 var HOST = 'http://hypem.com/';
 
 var DEFAULTS = {
-  cors: true,
-  withCredentials: true,
   method: 'GET',
   timeout: 30000
 };
@@ -2087,6 +2085,43 @@ exports.obsessed  = genProfileFunc('obsessed');
 
 var http = _dereq_('./http');
 
+var newImageRegex = /http:\/\/static-ak.hypem.net\/thumbs_new\/([^<]+).jpg/g
+  , trackRegex = /<script type="application\/json" id="displayList-data">([^<]+)<\/script>/;
+
+
+/**
+ * Find the thumbnial associated with the given track.
+ * @param {Object}
+ * @param {Array}
+ */
+function mapTrackThumb (track, thumbs) {
+  for (var i in thumbs) {
+    var postId = String(track.postid);
+    if (thumbs[i].indexOf(postId) !== -1) {
+      return thumbs[i];
+    }
+  }
+}
+
+
+/**
+ * Extract all thumbs and map them to the associated track
+ * @param {Object}
+ * @param {String}
+ */
+function mapThumbnails (tracks, body) {
+  var urls = body.match(newImageRegex)
+    , track = null;
+
+  for(var i in tracks.tracks) {
+    track = tracks.tracks[i];
+    track.thumb = mapTrackThumb(track, urls);
+  }
+
+  return tracks;
+}
+
+
 /**
  * Get the tracks for a given URL
  * @param {String}
@@ -2099,11 +2134,11 @@ exports.getTracksAtPath = function (path, callback) {
     }
 
     // Load standard hype pages via XHR then regex response for the tracks. Yuck
-    var tracks = body.match(/<script type="application\/json" id="displayList-data">([^<]+)<\/script>/);
+    var tracks = body.match(trackRegex)[1];
 
-    if (tracks && tracks[1]) {
+    if (tracks) {
       try {
-        return callback(null, res, JSON.parse(tracks[1]));
+        return callback(null, null, mapThumbnails(JSON.parse(tracks), body));
       } catch(e) {
         return callback('Received data in unexpected format.', res, null);
       }
@@ -2112,6 +2147,10 @@ exports.getTracksAtPath = function (path, callback) {
     }
   });
 };
+
+
+// need to regex out the background images too as they can't be resolved
+// from the JSON (I think) as they contain hex counter
 
 },{"./http":13}],19:[function(_dereq_,module,exports){
 'use strict';
